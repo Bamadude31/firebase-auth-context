1dbf89613886f42a7a0df352c9d9fd5ed182d030
Bill <aldridgewilliam01@gmail.com>
2023-12-04 20:08:07 -0700

    Fixed Login/Create acct page. DONT FUCK WITH

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div>
       <h1 className='text-center text-3xl font-bold'>
-        Firebase Auth & Context
+        Inventory Management App
       </h1>
       <AuthContextProvider>
         <Routes>
--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-apiKey: "YOUR UNIQUE API KEY",
-authDomain: "YOUR UNIQUE AUTHDOMAIN",
-projectId: "YOUR UNIQUE PROJECTID",
-storageBucket: "YOUR UNIQUE STORAGEBUCKET",
-messagingSenderId: "YOUR UNIQUE MESSAGINGSENDERID",
-appId: "YOUR UNIQUE API KEY"
-};
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCwdU75cniU7RU-aBZup_j4Vd5-pYPQkp8",
+  authDomain: "z-prefix-auth-development.firebaseapp.com",
+  projectId: "z-prefix-auth-development",
+  storageBucket: "z-prefix-auth-development.appspot.com",
+  messagingSenderId: "238321107611",
+  appId: "1:238321107611:web:fd7a1a2023f766c8c13c3e"
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export default app
-
-
-
